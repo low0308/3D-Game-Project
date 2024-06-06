dd40109e00cfb5b496256fa49b38ad486cdc0396
itsaidennn <wenjuntan42901@gmail.com>
2024-06-06 13:31:32 +0800

    3DPv0.2

---
+++ 3DPv0.1/Assets/3DGamekitLite/Scripts/Scripts/SirenLightRotation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SirenLightRotation : MonoBehaviour
+{
+    public float rotationSpeed = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+}
